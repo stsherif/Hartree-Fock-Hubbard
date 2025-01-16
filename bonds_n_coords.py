4a18a9cb0f577a251c6e634fcb62a1203a23268c
stsherif <115501295+stsherif@users.noreply.github.com>
2025-01-16 13:57:20 -0500

    Add files via upload

---
+++ bonds_n_coords.py
@@ -0,0 +1,125 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def lat_bonds(Nx,Ny):
+    N = Nx*Ny
+    bond=[]
+    bonds = []
+    for i in range(1,N):
+        if i%Ny==1 and i<N-Ny:
+            b=[i,i+1]
+            bond.append(b)
+            bonds.append(b)
+            b=[i,i+Ny]
+            bonds.append(b)
+            bond.append(b)
+            b=[i,i+Ny+1]
+            bonds.append(b)
+            bond.append(b)
+            b=[i,i+Ny-1]
+            #bonds.append(b)
+            bond.append(b)
+            b=[i,i+2*Ny-1]
+            #bonds.append(b)
+            bond.append(b)
+   
+        elif (i%Ny==0 and i<N):
+            b=[i,i+Ny]
+            bond.append(b)
+            bonds.append(b)
+
+        elif i>N-Ny:
+            b=[i,i+1]
+            bond.append(b)
+            bonds.append(b)
+            if i==N-Ny+1:
+                b=[i,i+Ny-1]
+                bond.append(b)
+
+        elif i%2==1:
+            b=[i,i+1]
+            bond.append(b)
+            bonds.append(b)
+            b=[i,i+Ny-1]
+            bond.append(b)
+            bonds.append(b)
+            b=[i,i+Ny]
+            bond.append(b)
+            bonds.append(b)
+            b=[i,i+Ny+1]
+            bond.append(b)
+            bonds.append(b)
+
+        elif i%2==0:
+            b=[i,i+1]
+            bond.append(b)
+            bonds.append(b)
+            b=[i,i+Ny]
+            bond.append(b)
+            bonds.append(b)
+    #I added this part to remove simillar elements from bond (only happens for Ny = 2)
+    unique_bond=sorted(set(tuple(b) for b in bond))
+    return unique_bond, bonds     #bond for cylindrical bc and bonds for open
+
+"""
+bond,_ = lat_bonds()
+
+for i  in range(len(bond)):
+    print(bond[i])
+"""
+
+#finding coordinates
+def coordinates(Nx,Ny):
+    N = Nx*Ny
+    a1 = np.array([1,0])
+    a2 = np.array([0.5,np.sqrt(3)/2.0])
+
+    Rxy = {}
+
+    for j in range(Ny,0,-1):
+        for i in range(1,Nx+1):
+            label = j+(i-1)*Ny
+            if j == 6:
+                Rxy[label] = (i-1)*a1
+            elif j==5:
+                Rxy[label] = (i-1)*a1 + a2
+            elif j==4:
+                Rxy[label] = (i-2)*a1 + 2*a2
+            elif j==3:
+                Rxy[label] = (i-2)*a1 + 3*a2
+            elif j==2:
+                Rxy[label] = (i-3)*a1 + 4*a2
+            else:
+                Rxy[label] = (i-3)*a1 + 5*a2
+    return Rxy
+
+'''
+Nx = 2
+Ny = 2
+N = Nx*Ny
+bond,bonds = lat_bonds(Nx,Ny)
+Rxy = coordinates(Nx,Ny)
+for k,bnd in enumerate(bonds):
+    s1=bnd[0]
+    s2=bnd[1]
+    
+    plt.plot([Rxy[s1][0],Rxy[s2][0]],[Rxy[s1][1],Rxy[s2][1]],lw=1.3,c='k')
+    plt.text(Rxy[s1][0]+0.05,Rxy[s1][1],str(s1),fontsize=10)#,weight="bold",color='green')
+#plt.text(Rxy[N][0]+0.05,Rxy[36][1],str(N),fontsize=10)
+
+#-----------------------------------------------temp------------just_for_a_pic-----------------------------
+
+for k,bnd in enumerate(bonds):
+    s1=bnd[0]
+    s2=bnd[1]
+    plt.scatter(Rxy[s1][0],Rxy[s1][1],s=80,c='k')
+ll=22    
+#plt.scatter(Rxy[ll][0],Rxy[ll][1],s=80,c='white')
+
+plt.axis("off")
+#plt.savefig('XC_6x6_1h.pdf',dpi=600,bbox_inches='tight')
+print(bond)
+print(Rxy)
+#plt.show()
+'''
---
+++ hartree_fock_hubbard.py
@@ -0,0 +1,125 @@
+import numpy as np
+import math
+import matplotlib.pyplot as pylab
+import sys
+import copy
+from bonds_n_coords import lat_bonds #triangular lattice rectangular geometery
+
+# This is for Hubbard Hamiltoinian
+# Mean Field Hamiltonian is H= KT + U sum_i ( <niup>nidn + niup<nidn> - <niup><nidn> - <Splusi>Sminsi - Splusi<Sminusi> + <Splusi><Sminsi> )
+# filling = 1 means half filling
+# trying with 4 sites triangular 2 ups 1 dn and 1 hole will generalize later
+if len(sys.argv) == 1:
+    print('Provide inputs: Nx Ny filling float U')
+    sys.exit(1)
+Nx = int(sys.argv[1])
+Ny = int(sys.argv[2])
+N = Nx * Ny
+filling = float(sys.argv[3])
+Np = int(filling * N)
+Nh = N - Np
+U = float(sys.argv[4])
+t = 1
+#print(Np)
+bonds, open_bonds = lat_bonds(Nx,Ny)
+#-------------------------------------------------------------------------------------------------------------------------------------------
+def fix_bonds(bonds): #this is different from my other fix_bonds function it depends on if it is (1up 2up ... 1dn 2dn ...) or (1up 1dn 2up 2dn ............) this one is for (1up 2up ......)
+    for i in range(len(bonds)):
+        bonds.append([bonds[i][0]+N,bonds[i][1]+N])
+    print("fixed_bonds = " ,bonds)
+    return bonds
+bonds = fix_bonds(bonds)
+#------------------------------------------------------------------------
+def is_hermitian(matrix):
+        return np.allclose(matrix, matrix.conj().T)
+#------------------------------------------------------------------------
+def hf (Hopping, nups, ndns, spluss):
+    N = int(len(Hopping[0])/2)
+    Hamiltonian = copy.deepcopy(Hopping)
+    #Make the intercating part-------------------------
+    for i in range(N):
+          Hamiltonian[i][i] += U * ndns[i] #niup <nidn> 
+          Hamiltonian[i+N][i+N] += U * nups[i] #<niup> nidn
+          Hamiltonian[i+N][i] += - U * spluss[i] #-<Splusi> Sminsi
+          Hamiltonian[i][i+N] += - U * spluss.conj()[i] #-Splusi <Sminsi>
+    #print(is_hermitian(Hamiltonian))
+    '''
+    for row in Hamiltonian:
+        formatted_row = " ".join(f"{element:3}" for element in row)
+        print(formatted_row)
+    '''
+    eigenvalues, eigenvectors = np.linalg.eigh(Hamiltonian)
+    #print(eigenvectors)
+    #print(eigenvectors[0][1])#, "    ", eigenvectors[0,1])
+    ef = eigenvalues[Np-1]
+    total_energy = 0.0
+    for i in range(Np):
+        total_energy += eigenvalues[i]
+    #------------------------------------Find the new prameters--------------------------------------------
+    nups_new = np.zeros(N)
+    ndns_new = np.zeros(N)
+    spluss_new = np.zeros(N, dtype=complex)
+    for i in range(N):
+         for j in range(Np):
+             if (eigenvectors[i][j] * (eigenvectors[i][j].conj())).imag < 0.0000001 and (eigenvectors[i+N][j] * (eigenvectors[i+N][j].conj())).imag < 0.0000001:
+                    nups_new[i] += (eigenvectors[i][j] * (eigenvectors[i][j].conj())).real
+                    ndns_new[i] += (eigenvectors[i+N][j] * (eigenvectors[i+N][j].conj())).real
+             else:
+                    print(" error : nups or udns is not real")
+             spluss_new[i] += eigenvectors[i+N][j] * (eigenvectors[i][j].conj())
+    for i in range(N):
+        total_energy += - U * nups[i] * ndns[i]
+        if (spluss[i] * spluss.conj()[i]).imag < 0.0000001:
+            total_energy += U * (spluss[i] * spluss.conj()[i]).real
+    return total_energy, nups_new, ndns_new, spluss_new         
+#-------------------------------------------------------------------------------------------------------------------------------
+Hopping = np.zeros((2*N,2*N), dtype=complex)
+#Make the non inteacting part----------------------
+for i in range(len(bonds)):
+    Hopping[bonds[i][0]-1][bonds[i][1]-1] = -t
+    Hopping[bonds[i][1]-1][bonds[i][0]-1] = -t
+se=1
+min_energy = 1000
+for seed in range(50):
+       np.random.seed(seed)
+       #------------------------Making up the random intial obeservables--------
+       nups_intial = np.random.rand(N)
+       ndns_intial = np.random.rand(N)
+       sumups = sum(nups_intial)
+       sumdns = sum(ndns_intial)
+       for i in range(len(nups_intial)):
+            nups_intial[i] = nups_intial[i] *math.ceil((0.5) *Np) / sumups
+            ndns_intial[i] = ndns_intial[i] *math.floor((0.5) *Np) / sumdns
+       mags_for_spluss = np.random.rand(N)
+       angles_for_spluss = np.random.rand(N) * 2 * np.pi # This is just to creat complex spluss with abs value = 1
+       spluss_intial = mags_for_spluss * (np.cos(angles_for_spluss) + 1j * np.sin(angles_for_spluss))
+       #print('intial up', sum(nups_intial))
+       #print('intial dn', sum(ndns_intial))
+       #print('intial Np', sum(nups_intial)+sum(ndns_intial))
+       nups = copy.deepcopy(nups_intial)
+       ndns = copy.deepcopy(ndns_intial)
+       spluss = copy.deepcopy(spluss_intial)
+       for iteration in range(20):
+            nups_old = copy.deepcopy(nups)
+            ndns_old = copy.deepcopy(ndns)
+            spluss_old = copy.deepcopy(spluss)
+            total_energy, nups_new, ndns_new, spluss_new = hf (Hopping, nups, ndns, spluss)
+            #print("E = ", total_energy)
+            if total_energy < min_energy:
+                min_energy = total_energy
+            for i in range(N):
+                nups[i] = 0.5 * (nups_old[i] + nups_new[i])
+                ndns[i] = 0.5 * (ndns_old[i] + ndns_new[i])
+                spluss[i] = 0.5 * (spluss_old[i] + spluss_new[i])
+       #print('E = ',total_energy)         
+print('--------------------------------------------------------')
+print('U = ',U)
+print('min energy',min_energy)
+print("nups = ",nups_new)
+print("ndns = ",ndns_new)
+print("spluss = ",spluss_new)
+print("sminss = ",np.conj(spluss_new))
+Nup = sum(nups_new)       
+Ndn = sum(ndns_new)  
+print("Nup = ",Nup,'   ','Ndn = ',Ndn,'   ','Np = ',Nup+Ndn)
+
